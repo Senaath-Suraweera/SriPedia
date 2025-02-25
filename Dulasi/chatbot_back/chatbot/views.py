7faf5188cb1d8859a53f54843e23ca36a693a921
Dulasi Keerthiratne <164042922+DulasiKeerthiratne@users.noreply.github.com>
2025-02-25 14:11:03 +0530

    feat: display user question and chatbot answer
    - Fixed issue where user input was not appearing in the chat UI
    - Implemented basic response handling so chatbot now replies

--- Dulasi/chatbot_back/chatbot/views.py
+++ Dulasi/chatbot_back/chatbot/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'Hi this is my response'
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
