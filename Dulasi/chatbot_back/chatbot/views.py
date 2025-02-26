3a0315ade0a1f730ec1badd2554a3925b6ab4563
Dulasi Keerthiratne <164042922+DulasiKeerthiratne@users.noreply.github.com>
2025-02-26 12:56:45 +0530

    chore: deleted all files to start over

--- Dulasi/chatbot_back/chatbot/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-def chatbot(request):
-    if request.method == 'POST':
-        message = request.POST.get('message')
-        response = 'Hi this is my response'
-        return JsonResponse({'message': message, 'response': response})
-    return render(request, 'chatbot.html')
