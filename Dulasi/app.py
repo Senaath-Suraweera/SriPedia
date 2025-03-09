3ea650dcbe39ae57efdef885d56c2f32de9d22e8
Dulasi Keerthiratne <164042922+DulasiKeerthiratne@users.noreply.github.com>
2025-03-09 09:01:25 +0530

    feat: split pdf text into chunks using langchain

--- Dulasi/app.py
+++ Dulasi/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -9,6 +10,11 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chuncks(text):
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len)
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def main():
     load_dotenv()
@@ -19,13 +25,17 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        pdf_docs = st.file_uploader("Upload your PDFs here and click 'Process'", accept_multiple_files=True)
+        pdf_docs = st.file_uploader("Upload your PDFs here and click 'Process'", type="pdf", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing..."):
                 #get pdf text chuncks
                 raw_text = get_pdf_text(pdf_docs)
                 st.write(raw_text)
+
                 #get the text chuncks
+                text_chuncks = get_text_chuncks(raw_text)
+                st.write(text_chuncks)
+
                 #create vector store
 
 if __name__ == '__main__':
