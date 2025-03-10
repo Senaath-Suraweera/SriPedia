bbdf71b90a4c48e5f9705f362d41c4cc5c30bb64
Janudiy <165156668+janudiy@users.noreply.github.com>
2025-03-10 21:26:20 +0530

    Functional Demo login and signup

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.sripediafront
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- authentication/templatetags/file_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def endswith(value, arg):
-    """Check if the value ends with the argument"""
-    return value.endswith(arg)
