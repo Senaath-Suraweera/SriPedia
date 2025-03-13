5d48690938a0aa3ef8d005a7790de6359d6e9df7
janudiy <janudiyatagampitiya@gmail.com>
2025-03-14 04:43:13 +0530

    added the language control page widgets

---
+++ lib/pages/language_page/language_widget.dart
@@ -0,0 +1,111 @@
+import '/flutter_flow/flutter_flow_radio_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'language_model.dart';
+export 'language_model.dart';
+
+class LanguageWidget extends StatefulWidget {
+  const LanguageWidget({super.key});
+
+  static String routeName = 'language';
+  static String routePath = '/language';
+
+  @override
+  State<LanguageWidget> createState() => _LanguageWidgetState();
+}
+
+class _LanguageWidgetState extends State<LanguageWidget> {
+  late LanguageModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => LanguageModel());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        appBar: AppBar(
+          backgroundColor: Color(0xFF6F61EF),
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Choose Language',
+            style: FlutterFlowTheme.of(context).headlineMedium.override(
+                  fontFamily: 'Inter Tight',
+                  color: Colors.white,
+                  fontSize: 22,
+                  letterSpacing: 0.0,
+                ),
+          ),
+          actions: [],
+          centerTitle: false,
+          elevation: 2,
+        ),
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Align(
+                alignment: AlignmentDirectional(0, 0),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(20, 60, 0, 0),
+                  child: FlutterFlowRadioButton(
+                    options: ['සිංහල', 'English', 'தமிழ்'].toList(),
+                    onChanged: (val) => safeSetState(() {}),
+                    controller: _model.radioButtonValueController ??=
+                        FormFieldController<String>(null),
+                    optionHeight: 100,
+                    optionWidth: 200,
+                    textStyle:
+                        FlutterFlowTheme.of(context).labelMedium.override(
+                              fontFamily: 'Inter',
+                              letterSpacing: 0.0,
+                            ),
+                    selectedTextStyle:
+                        FlutterFlowTheme.of(context).bodyMedium.override(
+                              fontFamily: 'Inter',
+                              letterSpacing: 0.0,
+                            ),
+                    buttonPosition: RadioButtonPosition.left,
+                    direction: Axis.vertical,
+                    radioButtonColor: Color(0xFF6F61EF),
+                    inactiveRadioButtonColor:
+                        FlutterFlowTheme.of(context).secondaryText,
+                    toggleable: false,
+                    horizontalAlignment: WrapAlignment.start,
+                    verticalAlignment: WrapCrossAlignment.start,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
