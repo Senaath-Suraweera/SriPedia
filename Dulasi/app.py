0feedf06d1ab6ea84eafd6bd4a0157ab3f053d6a
Dulasi Keerthiratne <164042922+DulasiKeerthiratne@users.noreply.github.com>
2025-03-15 02:16:54 +0530

    feat: add required LLM and database libraries

--- Dulasi/app.py
+++ Dulasi/app.py
@@ -2,3 +2,16 @@
 import json
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
+
+# llm imports
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import Qdrant
+from langchain_community.embeddings import OpenAIEmbeddings
+from qdrant_client import QdrantClient,models
+from qdrant_client.http.models import PointStruct
+import os
+from openai import OpenAI
+import uuid
+from qdrant_client.http.exceptions import UnexpectedResponse
+from flasgger import Swagger
