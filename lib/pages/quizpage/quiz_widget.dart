5ff77cb954c0c8a52a93b9e4d7bbd75a16e10d0b
janudiy <janudiyatagampitiya@gmail.com>
2025-03-15 02:34:05 +0530

    added quiz page

---
+++ lib/pages/quizpage/quiz_widget.dart
@@ -0,0 +1,12 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_timer.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'quizpage_model.dart';
+export 'quizpage_model.dart';
