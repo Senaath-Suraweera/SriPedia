e82daf8045ecd3bed0461d7635b2fe550befc826
Dulasi Keerthiratne <164042922+DulasiKeerthiratne@users.noreply.github.com>
2025-03-15 04:13:33 +0530

    feat: implement function to create Qdrant collection

--- Dulasi/app.py
+++ Dulasi/app.py
@@ -22,3 +22,25 @@ swagger = Swagger(app)
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def create_cluster_in_qdrant():
+    record=0
+    connection = QdrantClient(
+    url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY")
+)
+
+    try:
+        connection.get_collection("SriPedia")
+    except UnexpectedResponse as e:
+        connection.create_collection(
+            collection_name="SriPedia",
+            vectors_config=models.VectorParams(size=1536, distance=models.Distance.COSINE),
+        )  # Use create instead of recreate
+    
+
+    print("Create collection reponse:", connection)
+
+    info = connection.get_collection(collection_name="SriPedia")
+
+    return connection
